cc3887eca31a962749598f82732bf759db321d65
Markus Leist <markus@lei.st>
2016-07-15 11:30:37 +0200

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='ukmdb_graph',
-    version='0.0.5',
+    version='0.0.6',
     description="UKMDB graph database adapter.",
     long_description=readme + '\n\n' + history,
     author="Markus Leist",
